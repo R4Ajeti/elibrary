70cf47920bce9c30e3499967bb3050a9626b45d0
Jorge Apaza <yochiwarez@gmail.com>
2019-12-03 16:56:00 -0500

    added some funtionality

--- js/model.js
+++ js/model.js
@@ -1,26 +1,85 @@
 let myLibrary = [];
+var ul = document.querySelector("ul.books");
+
+let lb = localStorage.getItem('bookslist');
+
+if (lb){
+  myLibrary = JSON.parse(lb)
+}
 
 function Book(title, author, pages) {
   this.title = title,
-  this.author = author,
-  this.pages = pages,
-  this.read = false
+    this.author = author,
+    this.pages = pages,
+    this.read = false
 }
 
 function addBookToLibrary(title, author, pages) {
   myLibrary[myLibrary.length] = new Book(title, author, pages)
 }
 
-function displayAllBooks(){
-  let cloneLib = myLibrary
-  for(let i = 0; i<myLibrary.length; i++){
-      console.log("Title: "+cloneLib[i].title+" Author: "+cloneLib[i].author+" has: "+cloneLib[i].pages+" pages");
+function displayAllBooks() {
+
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
   }
-  console.log("true");
+
+  let cloneLib = myLibrary
+  for (let i = 0; i < myLibrary.length; i++) {
+    
+    let li = document.createElement("li");
+
+    li.setAttribute("index", i);
+    li.innerHTML = "<p>" + cloneLib[i].title + "</p>" +
+      "<p>" + cloneLib[i].author + "</p>" +
+      "<p>" + cloneLib[i].pages + "</p>";
+
+    let cb = document.createElement("input");
+    cb.setAttribute("type", "checkbox");
+    cb.setAttribute("onclick", "toggleReaded(" + i +")");
+    cb.checked = cloneLib[i].read;
+
+    li.appendChild(cb);
+
+    let a = document.createElement("a");
+    a.setAttribute("href", "#");
+    a.setAttribute("onclick", "removeBook(" + i + ")");
+    a.innerHTML = "Remove";
+
+    li.appendChild(a);
+
+    ul.appendChild(li);
+
+  }
+
+  localStorage.setItem('bookslist', JSON.stringify(myLibrary));
+
 }
 
-addBookToLibrary("Tesla", "Elon Musk", 193)
-addBookToLibrary("Google", "Just another author", 83)
-addBookToLibrary("Area", "Just another author", 83)
-console.log("MOdel is included!");
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+  displayAllBooks();
+}
+
+function toggleReaded(index){
+  let cb = document.querySelector(".books li[index='" + index + "'] input");
+  
+  myLibrary[index].read = cb.checked;
+
+  displayAllBooks();
+}
+
+btn = document.querySelector(".main_form .btn")
+
+btn.addEventListener("click", function () {
+
+  let title = document.querySelector(".main_form .title").value;
+  let author = document.querySelector(".main_form .author").value;
+  let pages = document.querySelector(".main_form .pages").value;
+
+  addBookToLibrary(title, author, pages);
+
+  displayAllBooks();
+})
+
 displayAllBooks();
